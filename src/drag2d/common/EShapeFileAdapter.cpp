ea72b63835493e0f36dc5ed0ed367a210fe1e534
ZZ <xzrunner@gmail.com>
2013-05-29 10:33:55 +0800

    fix body data

---
+++ src/drag2d/common/EShapeFileAdapter.cpp
@@ -0,0 +1,171 @@
+/*
+* Copyright (c) 2013 Guang Zhu http://runnersoft.net
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "EShapeFileAdapter.h"
+
+#include "common/Vector.h"
+#include "dataset/ChainShape.h"
+#include "dataset/RectShape.h"
+#include "dataset/CircleShape.h"
+
+using namespace d2d;
+
+EShapeFileAdapter::EShapeFileAdapter(std::vector<IShape*>& shapes)
+	: m_shapes(shapes)
+{
+}
+
+EShapeFileAdapter::~EShapeFileAdapter()
+{
+	for (size_t i = 0, n = m_shapes.size(); i < n; ++i)
+		m_shapes[i]->release();
+	m_shapes.clear();
+}
+
+void EShapeFileAdapter::load(const char* filename)
+{
+	Json::Value value;
+	Json::Reader reader;
+	std::ifstream fin(filename);
+	reader.parse(fin, value);
+	fin.close();
+
+	int i = 0;
+	Json::Value shapeValue = value["shapes"][i++];
+	while (!shapeValue.isNull()) {
+		IShape* shape = loadShape(shapeValue);
+		m_shapes.push_back(shape);
+		shapeValue = value["shapes"][i++];
+	}
+}
+
+void EShapeFileAdapter::store(const char* filename)
+{
+	Json::Value value;
+
+	for (size_t i = 0, n = m_shapes.size(); i < n; ++i)
+		value["shapes"][i] = store(m_shapes[i]);
+
+	Json::StyledStreamWriter writer;
+	std::ofstream fout(filename);
+	writer.write(fout, value);
+	fout.close();
+}
+
+IShape* EShapeFileAdapter::loadShape(const Json::Value& value)
+{
+	IShape* shape = NULL;
+
+	if (value.isNull())
+		;
+	else if (!value["chain"].isNull())
+		shape = loadChain(value["chain"]);
+	else if (!value["rect"].isNull())
+		shape = loadRect(value["rect"]);
+	else if (!value["circle"].isNull())
+		shape = loadCircle(value["circle"]);
+
+	return shape;
+}
+
+Json::Value EShapeFileAdapter::store(IShape* shape)
+{
+	Json::Value value;
+
+	if (ChainShape* chain = dynamic_cast<ChainShape*>(shape))
+		value["chain"] = store(chain);
+	else if (RectShape* rect = dynamic_cast<RectShape*>(shape))
+		value["rect"] = store(rect);
+	else if (CircleShape* circle = dynamic_cast<CircleShape*>(shape))
+		value["circle"] = store(circle);
+
+	return value;
+}
+
+IShape* EShapeFileAdapter::loadChain(const Json::Value& value)
+{
+	std::vector<Vector> vertices;
+	size_t num = value["vertices"]["x"].size();
+	vertices.resize(num);
+	for (size_t i = 0; i < num; ++i)
+	{
+		vertices[i].x = value["vertices"]["x"][i].asDouble();
+		vertices[i].y = value["vertices"]["y"][i].asDouble();
+	}
+
+	bool isLoop = value["closed"].asBool();
+
+	return new ChainShape(vertices, isLoop);
+}
+
+IShape* EShapeFileAdapter::loadRect(const Json::Value& value)
+{
+	const float xmin = value["xmin"].asDouble(),
+		xmax = value["xmax"].asDouble(),
+		ymin = value["ymin"].asDouble(),
+		ymax = value["ymax"].asDouble();
+
+	return new RectShape(Vector(xmin, ymin), Vector(xmax, ymax));
+}
+
+IShape* EShapeFileAdapter::loadCircle(const Json::Value& value)
+{
+	const float x = value["x"].asDouble(),
+		y = value["y"].asDouble(),
+		radius = value["radius"].asDouble();
+
+	return new CircleShape(Vector(x, y), radius);
+}
+
+Json::Value EShapeFileAdapter::store(const ChainShape* chain)
+{
+	Json::Value value;
+
+	const std::vector<Vector>& vertices = chain->getVertices();
+	for (size_t i = 0, n = vertices.size(); i < n; ++i)
+	{
+		value["vertices"]["x"][i] = vertices[i].x;
+		value["vertices"]["y"][i] = vertices[i].y;
+	}
+	value["closed"] = chain->isClosed();
+
+	return value;
+}
+
+Json::Value EShapeFileAdapter::store(const RectShape* rect)
+{
+	Json::Value value;
+
+	value["xmin"] = rect->m_rect.xMin;
+	value["xmax"] = rect->m_rect.xMax;
+	value["ymin"] = rect->m_rect.yMin;
+	value["ymax"] = rect->m_rect.yMax;
+
+	return value;
+}
+
+Json::Value EShapeFileAdapter::store(const CircleShape* circle)
+{
+	Json::Value value;
+
+	value["x"] = circle->center.x;
+	value["y"] = circle->center.y;
+	value["radius"] = circle->radius;
+
+	return value;
+}
---
+++ src/drag2d/dataset/ShapesBody.cpp
@@ -0,0 +1,100 @@
+/*
+* Copyright (c) 2012-2013 Guang Zhu http://runnersoft.net
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include <Box2D/Box2D.h>
+
+#include "ShapesBody.h"
+#include "BodyData.h"
+
+#include "common/Math.h"
+
+using namespace d2d;
+
+ShapesBody::ShapesBody(b2World* world, const BodyData& bodyData, float scale/* = 1.0f*/)
+	: IBody(world, scale)
+{
+	m_filename = bodyData.getFilename();
+
+	b2BodyDef bd;
+	bd.type = b2_dynamicBody;
+	m_body = world->CreateBody(&bd);
+
+	const std::vector<BodyData::FixtureData*>& fixturesData = bodyData.getAllFixturesData();
+	for (size_t i = 0, n = fixturesData.size(); i < n; ++i)
+	{
+		const BodyData::FixtureData* fixtureData = fixturesData[i];
+
+		const size_t size = fixtureData->vertices.size();
+		if (size == 1)
+		{
+			b2FixtureDef fd;
+			fd.density = 1;
+			fd.restitution = 0.6f;
+
+			b2CircleShape shape;
+			shape.m_radius = fixtureData->vertices[0].x * 0.5f * scale / BOX2D_SCALE_FACTOR;
+			fd.shape = &shape;
+
+			m_body->CreateFixture(&fd);
+		}
+		else
+		{
+			std::vector<b2Vec2> transList(size);
+			for (size_t j = 0, m = size; j < m; ++j)
+			{
+				transList[j].x = fixtureData->vertices[j].x * scale / BOX2D_SCALE_FACTOR;
+				transList[j].y = fixtureData->vertices[j].y * scale / BOX2D_SCALE_FACTOR;
+			}
+
+			b2PolygonShape shape;
+			shape.Set(&transList[0], size);
+
+			b2FixtureDef fd;
+			fd.shape = &shape;
+			fd.friction = 0.5f;
+			fixtureData->setFixtureDef(fd);
+
+			m_body->CreateFixture(&fd);
+		}
+	}
+}
+
+void ShapesBody::getRect(Rect& rect, const Vector& position, float angle) const
+{
+	rect.makeInfinite();
+	for (b2Fixture* f = m_body->GetFixtureList(); f; f = f->GetNext())
+	{
+		if (b2PolygonShape* poly = dynamic_cast<b2PolygonShape*>(f->GetShape()))
+		{
+			assert(poly);
+			for (size_t i = 0; i < poly->m_vertexCount; ++i)
+			{
+				const b2Vec2& p = poly->m_vertices[i];
+				Vector transform = Math::rotateVector(Vector(p.x, p.y), angle) + position;
+				rect.combine(transform);
+			}
+		}
+		else if (b2Shape* shape = m_body->GetFixtureList()->GetShape())
+		{
+			b2CircleShape* circle = dynamic_cast<b2CircleShape*>(shape);
+			assert(circle);
+			rect.combine(d2d::Vector(position.x - circle->m_radius, position.y - circle->m_radius));
+			rect.combine(d2d::Vector(position.x + circle->m_radius, position.y + circle->m_radius));
+		}
+	}
+}
