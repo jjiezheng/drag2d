83afba82d08787190fbc8aa899e8595ffb9ff626
zz <zhuguang@ejoy.com>
2013-10-22 10:59:07 +0800

    fix SpriteDropTarget

--- src/drag2d/view/SpriteDropTarget.cpp
+++ src/drag2d/view/SpriteDropTarget.cpp
@@ -6,7 +6,7 @@
 #include "dataset/SpriteFactory.h"
 #include "view/LibraryPanel.h"
 #include "view/EditPanel.h"
-#include "view/SpritePanelImpl.h"
+#include "view/SpritesPanelImpl.h"
 
 namespace d2d
 {
